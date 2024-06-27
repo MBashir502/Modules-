a5523b8231d1ecd11e48dc32e0337d5d86a9c562
Muhammad Bashir <muhammadbashir0324@gmail.com>
2024-06-27 09:10:57 +0500

    normalize

--- Path_Module/index.js
+++ Path_Module/index.js
@@ -5,6 +5,8 @@ const path = require("path");
 // console.log(path.dirname("f:/NODEJS-Modules/Wrapper_Module/index.js"));
 // console.log(path.dirname(__filename));
 // console.log(path.extname(__filename));
-console.log(path.join('/search',"home","about/contact","info","view"));
-console.log(path.join('/search',"home","about/contact","info","view",".."));
-console.log(path.join(__dirname,"home","about/contact","info","view",".."));
+
+// console.log(path.join('/search',"home","about/contact","info","view"));
+// console.log(path.join('/search',"home","about/contact","info","view",".."));
+// console.log(path.join(__dirname,"home","about/contact","info","view",".."));
+console.log(path.normalize("f:\\mbt\\ali\\\\temp"));
